f988335f3cecd1f547b496a63f5437e206143d10
Seba Flomenbaun <158225487+duncann555@users.noreply.github.com>
2025-06-26 17:10:22 -0300

    se realizo validacion de 0 ingresos de ciudades para ejercicio 2

--- js/Ejercicio2.js
+++ js/Ejercicio2.js
@@ -17,52 +17,69 @@ let ciudad;
 do {
   ciudad = prompt("Ingrese el nombre de una ciudad (o cancele para terminar):");
   if (ciudad) {
-    ciudades.push(ciudad.toUpperCase()); //El UpperCase lo puse solo por estetica 
+    ciudades.push(ciudad.toUpperCase()); //El UpperCase lo puse solo por estetica
   }
 } while (ciudad);
 
-document.writeln("<h3>A. Arreglo generado:</h3>");
-document.writeln("<ul>");
-for (let i = 0; i < ciudades.length; i++) {
-  document.writeln(`<li>${ciudades[i]}</li>`);
-}
-document.writeln("</ul>");
-
-document.writeln(`<h3>B. El arreglo de ciudades tiene : ${ciudades.length} elementos</h3>`);
-
-document.writeln(`<h3>C. Items de la posicion 1,3 y ultima:</h3>`);
-if (ciudades.length > 0) {
-  document.writeln(`<p>Posicion 1: ${ciudades[0]}</p>`);
-  if (ciudades.length > 2) {
-    document.writeln(`<p>Posicion 3: ${ciudades[2]}</p>`);
-  } else {
-    document.writeln(`<p>Posicion 3: No hay ciudad en posicion 3 ingresada.</p>`);
+if (ciudades.length !== 0) {
+  document.writeln("<h3>A. Arreglo generado:</h3>");
+  document.writeln("<ul>");
+  for (let i = 0; i < ciudades.length; i++) {
+    document.writeln(`<li>${ciudades[i]}</li>`);
   }
-  document.writeln(`<p>Ultima posicion: ${ciudades[ciudades.length - 1]}</p>`);
+  document.writeln("</ul>");
+
+  document.writeln(
+    `<h3>B. El arreglo de ciudades tiene : ${ciudades.length} elementos</h3>`
+  );
+
+  document.writeln(`<h3>C. Items de la posicion 1,3 y ultima:</h3>`);
+  if (ciudades.length > 0) {
+    document.writeln(`<p>Posicion 1: ${ciudades[0]}</p>`);
+    if (ciudades.length > 2) {
+      document.writeln(`<p>Posicion 3: ${ciudades[2]}</p>`);
+    } else {
+      document.writeln(
+        `<p>Posicion 3: No hay ciudad en posicion 3 ingresada.</p>`
+      );
+    }
+    document.writeln(
+      `<p>Ultima posicion: ${ciudades[ciudades.length - 1]}</p>`
+    );
+  }
+
+  ciudades.push("París".toUpperCase());
+  document.writeln(
+    `<h3>D. Ciudad añadida al final:</h3><p>${
+      ciudades[ciudades.length - 1]
+    }</p>`
+  );
+
+  if (ciudades.length > 1) {
+    document.writeln(
+      `<h3>E. Ciudad en segunda posición:</h3><p>${ciudades[1]}</p>`
+    );
+  } else {
+    document.writeln(`<h3>E. No hay una segunda ciudad para mostrar</h3>`);
+  }
+
+  if (ciudades.length > 1) {
+    ciudades[1] = "Barcelona".toUpperCase();
+    document.writeln(
+      `<h3>F. Ciudad en segunda posición cambiada a:</h3><p>${ciudades[1]}</p>`
+    );
+  } else {
+    document.writeln(
+      `<h3>F. No se pudo reemplazar la segunda ciudad porque no existe</h3>`
+    );
+  }
+
+  document.writeln(`<h3>G. Contenido final del arreglo:</h3>`);
+  document.writeln("<ul>");
+  for (let i = 0; i < ciudades.length; i++) {
+    document.writeln(`<li>${ciudades[i]}</li>`);
+  }
+  document.writeln("</ul>");
 } else {
-  document.writeln(`<p>No se ingresaron ciudades.</p>`);
+  document.writeln("<h3>No se ingresaron ciudades.</h3>");
 }
-
-ciudades.push("París".toUpperCase());
-document.writeln(`<h3>D. Ciudad añadida al final:</h3><p>${ciudades[ciudades.length - 1]}</p>`);
-
-if (ciudades.length > 1) {
-  document.writeln(`<h3>E. Ciudad en segunda posición:</h3><p>${ciudades[1]}</p>`);
-} else {
-  document.writeln(`<h3>E. No hay una segunda ciudad para mostrar</h3>`);
-}
-
-if (ciudades.length > 1) {
-  ciudades[1] = "Barcelona".toUpperCase();
-  document.writeln(`<h3>F. Ciudad en segunda posición cambiada a:</h3><p>${ciudades[1]}</p>`);
-} else {
-  document.writeln(`<h3>F. No se pudo reemplazar la segunda ciudad porque no existe</h3>`);
-}
-
-document.writeln(`<h3>G. Contenido final del arreglo:</h3>`);
-document.writeln("<ul>");
-for (let i = 0; i < ciudades.length; i++) {
-  document.writeln(`<li>${ciudades[i]}</li>`);
-}
-document.writeln("</ul>");
-
